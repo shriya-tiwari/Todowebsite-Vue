328aa2a0a54f55b2647ce574ffb822d00e060285
yash2002109 <yash2002109@gmail.com>
2022-06-20 02:46:46 +0530

    Add correcting files
    Signed-off-by: yash2002109 <yash2002109@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vuejs-accessibility/recommended',
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  plugins: ['vuejs-accessibility', 'prettier'],
+  // add your custom rules here
+  rules: {
+    'no-console': 'warn',
+    'vue/singleline-html-element-content-newline': 0,
+    'vue/html-self-closing': 0,
+    'vue/html-closing-bracket-newline': 0,
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  globals: {
+    $nuxt: true,
+  },
+}
